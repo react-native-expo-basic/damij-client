c43cd7d5c2766bdbd130d02e41834f70e9bf7380
minseung-gang <strause1@nate.com>
2023-05-16 00:02:02 +0900

    Update: expo에 폰트 적용하는 코드 추가

--- App.tsx
+++ App.tsx
@@ -1,7 +1,21 @@
 import { ThemeProvider } from "react-native-rapi-ui";
 import AppNavigator from "./src/navigation/AppNavigator";
+import React, { useEffect } from "react";
+import * as Font from "expo-font";
 
 export default function App() {
+  useEffect(() => {
+    const loadFonts = async () => {
+      await Font.loadAsync({
+        "Noto-Sans-Regular": require("./assets/fonts/NotoSansKR-Regular.otf"),
+        "Noto-Sans-Medium": require("./assets/fonts/NotoSansKR-Medium.otf"),
+        "Noto-Sans-Bold": require("./assets/fonts/NotoSansKR-Bold.otf"),
+      });
+    };
+
+    loadFonts();
+  }, []);
+
   return (
     <ThemeProvider>
       <AppNavigator></AppNavigator>
