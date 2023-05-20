fe89f2987881164155773376b3e1846817ee2b29
minseung-gang <strause1@nate.com>
2023-05-20 12:29:46 +0900

    Add: 하단탭바 클릭 시 아이콘 색상 및 아이콘 수정 기능 추가

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,38 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
+import { Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
+import { Ionicons } from "@expo/vector-icons";
 import Home from "../screens/Home";
-import Login from "../screens/Login";
-
-import SignupContainer from "../container/SignupContainer";
+import Category from "../screens/Category";
+import Likes from "../screens/Likes";
+import Mypage from "../screens/Mypage";
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
-
-const StackNavigator = () => {
-  return (
-    <Stack.Navigator
-      screenOptions={{ headerShown: false }}
-      initialRouteName="Login"
-    >
-      <Stack.Screen name="Login" component={Login} />
-      <Stack.Screen name="SignupContainer" component={SignupContainer} />
-    </Stack.Navigator>
-  );
-};
+type IoniconsName =
+  | "home-outline"
+  | "home-sharp"
+  | "person-sharp"
+  | "person-outline"
+  | "ios-heart-outline"
+  | "ios-heart-sharp"
+  | "menu-outline";
 
 const Main = () => {
+  const [selectedTab, setSelectedTab] = useState("Home");
   return (
     <Tab.Navigator
       initialRouteName="Home"
-      screenOptions={{
+      screenOptions={({ route }) => ({
         headerShown: false,
-      }}
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName: IoniconsName = "home-sharp";
+
+          if (route.name === "홈") {
+            iconName = focused ? "home-sharp" : "home-outline";
+          } else if (route.name === "카테고리") {
+            iconName = "menu-outline";
+          } else if (route.name === "찜") {
+            iconName = focused ? "ios-heart-sharp" : "ios-heart-outline";
+          } else if (route.name === "마이페이지") {
+            iconName = focused ? "person-sharp" : "person-outline";
+          }
+
+          const iconColor = focused ? "#ff5a5a" : "grey";
+
+          return <Ionicons name={iconName} size={size} color={iconColor} />;
+        },
+        tabBarLabel: ({ focused, color }) => {
+          const labelColor = focused ? "#ff5a5a" : "grey";
+
+          return (
+            <Text style={{ color: labelColor, fontSize: 12 }}>
+              {route.name}
+            </Text>
+          );
+        },
+        tabBarStyle: {
+          height: 50, // 탭 바의 높이를 조절합니다.
+        },
+      })}
     >
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="StackNavigator" component={StackNavigator} />
+      <Tab.Screen name="홈" component={Home} />
+      <Tab.Screen name="카테고리" component={Category} />
+      <Tab.Screen name="찜" component={Likes} />
+      <Tab.Screen name="마이페이지" component={Mypage} />
     </Tab.Navigator>
   );
 };
