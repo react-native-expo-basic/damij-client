e304c3bfd0856bc185c2082d4a83560ba7d415cb
minseung-gang <strause1@nate.com>
2023-05-22 20:42:45 +0900

    feat: 홈 카테고리의 상단탭 바에 들어가는 컴포넌트 추가

---
+++ src/components/home/SaleItems.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function SaleItems() {
+  return (
+    <View>
+      <Text>SaleItems</Text>
+    </View>
+  );
+}
