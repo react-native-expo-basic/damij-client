436edb0621b9b041a63ab53d0aeb183325e6b6e2
minseung-gang <strause1@nate.com>
2023-05-22 20:44:32 +0900

    feat: 메인화면에 상단 탭바 추가 및 관련 기능 추가

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,64 @@
-import React from "react";
-import { View, Text } from "react-native";
+import * as React from "react";
+import { useState } from "react";
+import Main from "../components/home/Main";
+import { View, Text, useWindowDimensions } from "react-native";
+import { TabBar, TabView, SceneMap } from "react-native-tab-view";
+import { TabBarProps } from "react-native-tab-view";
+import BestItems from "../components/home/BestItems";
+import NewItems from "../components/home/NewItems";
+import SaleItems from "../components/home/SaleItems";
+import styled from "styled-components/native";
+
+const HomeCategory = () => <Main />;
+const BestItemsCategory = () => <BestItems />;
+const NewItemsCategory = () => <NewItems />;
+const SalesCategory = () => <SaleItems />;
+
+const renderScene = SceneMap({
+  home: HomeCategory,
+  best: BestItemsCategory,
+  new: NewItemsCategory,
+  sale: SalesCategory,
+});
+interface TabTextProps {
+  focused: boolean;
+}
 export default function Home() {
+  const layout = useWindowDimensions();
+  const [index, setIndex] = useState(0);
+  const [routes] = useState([
+    { key: "home", title: "홈" },
+    { key: "best", title: "BEST" },
+    { key: "new", title: "NEW" },
+    { key: "sale", title: "SALE" },
+  ]);
+
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={{ flex: 1, backgroundColor: "white" }}>
+      <TabView
+        renderTabBar={(props) => (
+          <TabBar
+            {...props}
+            style={{ backgroundColor: "unset" }}
+            labelStyle={{ color: "black" }}
+            pressColor="transparent"
+            indicatorStyle={{ backgroundColor: "black" }}
+            renderLabel={({ route, focused }) => (
+              <TabText focused={focused}>{route.title}</TabText>
+            )}
+          />
+        )}
+        navigationState={{ index, routes }}
+        renderScene={renderScene}
+        onIndexChange={setIndex}
+        initialLayout={{ width: layout.width }}
+      />
     </View>
   );
 }
+const TabText = styled.Text<TabTextProps>`
+  color: ${(props) => (props.focused ? "black" : "grey")};
+  margin: 8px;
+  font-weight: ${(props) => (props.focused ? "600" : "500")};
+  font-size: 16px;
+`;
