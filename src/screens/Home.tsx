6c10cec77decd481caf5ee259bf26766a6296e07
minseung-gang <strause1@nate.com>
2023-05-24 19:45:42 +0900

    Add: 서버로부터 데이터 fetch 후 하위 탭 컴포넌트로 전달 및 관련 타입지정

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,37 +1,56 @@
 import * as React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Main from "../components/home/Main";
 import { View, Text, useWindowDimensions } from "react-native";
 import { TabBar, TabView, SceneMap } from "react-native-tab-view";
-import { TabBarProps } from "react-native-tab-view";
 import BestItems from "../components/home/BestItems";
 import NewItems from "../components/home/NewItems";
 import SaleItems from "../components/home/SaleItems";
 import styled from "styled-components/native";
+import { fetchProductData } from "../utils/fetchProductData";
+import { ProductType } from "../types/types";
 
-const HomeCategory = () => <Main />;
-const BestItemsCategory = () => <BestItems />;
-const NewItemsCategory = () => <NewItems />;
-const SalesCategory = () => <SaleItems />;
-
-const renderScene = SceneMap({
-  home: HomeCategory,
-  best: BestItemsCategory,
-  new: NewItemsCategory,
-  sale: SalesCategory,
-});
 interface TabTextProps {
   focused: boolean;
 }
+
 export default function Home() {
-  const layout = useWindowDimensions();
+  const layout = useWindowDimensions(); //TabView 컴포넌트에서 초기 레이아웃 설정을 위해서
   const [index, setIndex] = useState(0);
+  const [productInfo, setProductInfo] = useState<ProductType[]>([]);
   const [routes] = useState([
     { key: "home", title: "홈" },
     { key: "best", title: "BEST" },
     { key: "new", title: "NEW" },
     { key: "sale", title: "SALE" },
   ]);
+
+  const HomeCategory = () => <Main productInfo={productInfo} />;
+  const BestItemsCategory = () => <BestItems productInfo={productInfo} />;
+  const NewItemsCategory = () => <NewItems productInfo={productInfo} />;
+  const SalesCategory = () => <SaleItems productInfo={productInfo} />;
+
+  const renderScene = SceneMap({
+    home: HomeCategory,
+    best: BestItemsCategory,
+    new: NewItemsCategory,
+    sale: SalesCategory,
+  });
+
+  const fetchDataFromServer = async () => {
+    try {
+      const response = await fetchProductData();
+      /* const isNewItems = filterdIsNew(response);
+      const isBestItems = filterdIsBest(response); */
+      setProductInfo(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchDataFromServer();
+  }, []);
 
   return (
     <View style={{ flex: 1, backgroundColor: "white" }}>
