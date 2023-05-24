b9f509c019f5adf8800266382e86a8cc7e398f5f
minseung-gang <strause1@nate.com>
2023-05-24 19:48:07 +0900

    Add: 상품관련 타입지정 추가

--- src/types/types.ts
+++ src/types/types.ts
@@ -2,11 +2,28 @@ export type SignupReqType = {
   email: string;
   password: string;
   /*   name: string; */
-  /*  address: string; */
-  /*   phoneNumb: number; */
+  /*   address: string;
+  phoneNumb: number; */
 };
 
 export type LoginReqType = {
   email: string;
   password: string;
 };
+
+//product type
+export interface ProductType {
+  image: string;
+  product_name: string;
+  product_price: string;
+  product_color?: string;
+  discount_rate: number;
+  isOnSale: boolean;
+  salesCount: number;
+  isNew: boolean;
+  review_count: number;
+}
+
+export interface MainProps {
+  productInfo: ProductType[];
+}
