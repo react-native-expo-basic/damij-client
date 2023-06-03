c0337eda096ec2084f3ce0e19384942323067dee
minseung-gang <strause1@nate.com>
2023-06-03 23:50:54 +0900

    Feat: 모달 관련 리듀서 추가

--- src/redux/modules/reducer.ts
+++ src/redux/modules/reducer.ts
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 import likes from "./likes";
+import modalState from "./modal";
 const reducer = combineReducers({
   likes,
+  modal: modalState,
 });
 
 export default reducer;
