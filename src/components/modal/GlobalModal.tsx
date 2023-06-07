df91973c45d8c2962b5731a584898900252b8403
minseung-gang <strause1@nate.com>
2023-06-07 20:49:05 +0900

    Feat: 모달을 전역관리를 위한 컴포넌트화

--- src/components/modal/GlobalModal.tsx
+++ src/components/modal/GlobalModal.tsx
@@ -1,24 +1,25 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Modal, View } from "react-native";
-import modalState from "redux/modules/modal";
-import useModal from "hooks/useModal";
-import AlarmModal from "./AlarmModal";
-import { modalActionType } from "redux/modules/modal";
-export default function GlobalModal() {
-  const { modalType, isOpen, props } = useSelector(
-    (state: modalActionType) => state.modal
-  );
-  const { openModal, closeModal } = useModal();
+import modalState from "../../redux/modules/modal";
+import { ModalActionType } from "redux/modules/modal";
+import LikeNotification from "./LikeNotification";
 
-  if (!isOpen) return;
+interface ModalState {
+  modal: ModalActionType;
+}
+
+export default function GlobalModal(): JSX.Element {
+  const { modalType, isOpen, props } = useSelector(
+    (state: ModalState) => state.modal
+  );
+
+  if (!isOpen) {
+    return <></>;
+  }
 
   const MODAL_COMPONENTS: { [key: string]: JSX.Element } = {
-    alarm: <AlarmModal {...props} />,
+    alarm: <LikeNotification props={props} />,
   };
-  return (
-    <Modal>
-      <View>{MODAL_COMPONENTS[modalType]}</View>
-    </Modal>
-  );
+  return <View>{MODAL_COMPONENTS[modalType]}</View>;
 }
