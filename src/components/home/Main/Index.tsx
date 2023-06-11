af30fa5969bb42b18c84b6e3993a00a08868f689
minseung-gang <strause1@nate.com>
2023-06-11 18:12:40 +0900

    Fix: FlatList 적용

--- src/components/home/Main/Index.tsx
+++ src/components/home/Main/Index.tsx
@@ -1,14 +1,27 @@
 import React from "react";
 import SwiperComponent from "./SwiperComponent";
 import ProductList from "./ProductList";
-import { MainProps } from "../../../types/types";
+import { MainProps, DataType } from "../../../types/types";
 import Section from "../Section";
+import { FlatList } from "react-native";
 
 export default function Index({ productInfo }: MainProps) {
+  const data = [{ id: "swiper" }, { id: "productList" }];
+
+  const renderItem = ({ item }: { item: DataType }) => {
+    switch (item.id) {
+      case "swiper":
+        return <SwiperComponent />;
+      case "productList":
+        return <ProductList productInfo={productInfo} />;
+      default:
+        return null;
+    }
+  };
+
   return (
     <Section>
-      <SwiperComponent />
-      <ProductList productInfo={productInfo} />
+      <FlatList data={data} renderItem={renderItem} />
     </Section>
   );
 }
--- src/components/home/new/Index.tsx
+++ src/components/home/new/Index.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 import Category from "../../Category";
-import { MainProps } from "../../../types/types";
+import { filteredIsNew } from "../../../utils/productUtils";
+import { MainProps, DataType } from "../../../types/types";
 import Product from "../main/Product";
-import { filteredIsBest } from "../../../utils/productUtils";
 import Section from "../Section";
 import styled from "styled-components/native";
 import { FlatList } from "react-native";
-import { DataType } from "types/types";
 
 export default function Index({ productInfo }: MainProps) {
-  const popularProducts = filteredIsBest(productInfo);
+  const newProducts = filteredIsNew(productInfo);
   const data = [{ id: "category" }, { id: "product" }];
 
   const renderItem = ({ item }: { item: DataType }) => {
@@ -17,11 +16,12 @@ export default function Index({ productInfo }: MainProps) {
       case "category":
         return <Category />;
       case "product":
-        return <Product products={popularProducts} />;
+        return <Product products={newProducts} />;
       default:
         return null;
     }
   };
+
   return (
     <Section>
       <PaddingView>
