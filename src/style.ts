4800b0a84701317a7637ac97a4200244ddaed3cd
minseung-gang <strause1@nate.com>
2023-06-12 16:38:41 +0900

    styles: 상품 관련 텍스트 스타일 추가

--- src/style.ts
+++ src/style.ts
@@ -3,3 +3,9 @@ const DISABLED_COLOR2 = "#bcbcbc";
 
 export const viewDisableColor = DISABLED_COLOR;
 export const textDisableColor = DISABLED_COLOR2;
+export const textOriginalPriceColor = DISABLED_COLOR2;
+export const textPriceFontSize = "16px";
+export const textOriginalPriceFontSize = "15px";
+export const textPriceName = "12px";
+export const textSalePriceName = "14px";
+export const textSalePriceFontSize = "20px";
