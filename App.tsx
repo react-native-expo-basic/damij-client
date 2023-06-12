5f125ddbd80dc04daf993cfee6fc635d97af07ea
minseung-gang <strause1@nate.com>
2023-06-12 16:44:51 +0900

    Update: 리덕스 사용하기 위한 스토어 및 폰트 추가

--- App.tsx
+++ App.tsx
@@ -3,9 +3,14 @@ import AppNavigator from "./src/navigation/AppNavigator";
 import React, { useEffect, useState } from "react";
 import * as Font from "expo-font";
 import Header from "./src/components/Header";
+import create from "./src/redux/modules/create";
+import { Provider } from "react-redux";
+import GlobalModal from "./src/components/modal/GlobalModal";
 
 export default function App() {
   const [fontsLoaded, setFontsLoaded] = useState(false);
+  const store = create();
+
   useEffect(() => {
     const loadFonts = async () => {
       await Font.loadAsync({
@@ -13,6 +18,7 @@ export default function App() {
         "Noto-Sans-Medium": require("./assets/fonts/NotoSansKR-Medium.otf"),
         "Noto-Sans-Bold": require("./assets/fonts/NotoSansKR-Bold.otf"),
         "Montserrat-SemiBold": require("./assets/fonts/Montserrat-SemiBold.ttf"),
+        "Montserrat-Regular": require("./assets/fonts/Montserrat-Regular.ttf"),
       });
     };
 
@@ -25,9 +31,12 @@ export default function App() {
   }
 
   return (
-    <ThemeProvider>
-      <Header />
-      <AppNavigator></AppNavigator>
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider>
+        <Header />
+        <AppNavigator />
+        <GlobalModal />
+      </ThemeProvider>
+    </Provider>
   );
 }
