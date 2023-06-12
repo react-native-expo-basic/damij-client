36098a0896fb8adec5390b12627d2ced8ef4db75
minseung-gang <strause1@nate.com>
2023-06-12 16:42:25 +0900

    Update: 전역 모달 추가

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Ionicons } from "@expo/vector-icons";
@@ -7,6 +7,7 @@ import Home from "../screens/Home";
 import Category from "../screens/Category";
 import Likes from "../screens/Likes";
 import Mypage from "../screens/Mypage";
+import GlobalModal from "../components/modal/GlobalModal";
 
 const Tab = createBottomTabNavigator();
 
@@ -20,55 +21,59 @@ type IoniconsName =
   | "menu-outline";
 
 const Main = () => {
-  const [selectedTab, setSelectedTab] = useState("Home");
   return (
-    <Tab.Navigator
-      initialRouteName="Home"
-      screenOptions={({ route }) => ({
-        headerShown: false,
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName: IoniconsName = "home-sharp";
+    <View style={{ flex: 1 }}>
+      <Tab.Navigator
+        initialRouteName="Home"
+        screenOptions={({ route }) => ({
+          headerShown: false,
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName: IoniconsName = "home-sharp";
 
-          if (route.name === "홈") {
-            iconName = focused ? "home-sharp" : "home-outline";
-          } else if (route.name === "카테고리") {
-            iconName = "menu-outline";
-          } else if (route.name === "찜") {
-            iconName = focused ? "ios-heart-sharp" : "ios-heart-outline";
-          } else if (route.name === "마이페이지") {
-            iconName = focused ? "person-sharp" : "person-outline";
-          }
+            if (route.name === "홈") {
+              iconName = focused ? "home-sharp" : "home-outline";
+            } else if (route.name === "카테고리") {
+              iconName = "menu-outline";
+            } else if (route.name === "찜") {
+              iconName = focused ? "ios-heart-sharp" : "ios-heart-outline";
+            } else if (route.name === "마이페이지") {
+              iconName = focused ? "person-sharp" : "person-outline";
+            }
 
-          const iconColor = focused ? "#ff5a5a" : "grey";
+            const iconColor = focused ? "#ff5a5a" : "grey";
 
-          return <Ionicons name={iconName} size={size} color={iconColor} />;
-        },
-        tabBarLabel: ({ focused }) => {
-          const labelColor = focused ? "#ff5a5a" : "grey";
+            return <Ionicons name={iconName} size={size} color={iconColor} />;
+          },
+          tabBarLabel: ({ focused }) => {
+            const labelColor = focused ? "#ff5a5a" : "grey";
 
-          return (
-            <Text style={{ color: labelColor, fontSize: 12 }}>
-              {route.name}
-            </Text>
-          );
-        },
-        tabBarStyle: {
-          height: 50, // 탭 바의 높이를 조절합니다.
-        },
-      })}
-    >
-      <Tab.Screen name="홈" component={Home} />
-      <Tab.Screen name="카테고리" component={Category} />
-      <Tab.Screen name="찜" component={Likes} />
-      <Tab.Screen name="마이페이지" component={Mypage} />
-    </Tab.Navigator>
+            return (
+              <Text style={{ color: labelColor, fontSize: 12 }}>
+                {route.name}
+              </Text>
+            );
+          },
+          tabBarStyle: {
+            height: 50, // 탭 바의 높이를 조절합니다.
+          },
+        })}
+      >
+        <Tab.Screen name="홈" component={Home} />
+        <Tab.Screen name="카테고리" component={Category} />
+        <Tab.Screen name="찜" component={Likes} />
+        <Tab.Screen name="마이페이지" component={Mypage} />
+      </Tab.Navigator>
+    </View>
   );
 };
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Main />
+      <View style={{ flex: 1, position: "relative" }}>
+        <Main />
+        <GlobalModal />
+      </View>
     </NavigationContainer>
   );
 };
