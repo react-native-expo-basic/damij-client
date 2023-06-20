1a8568a640d640cf806e6297bbd7bd625ab1faca
minseung-gang <strause1@nate.com>
2023-06-20 19:39:45 +0900

    Update: props타입 옵셔널 체이닝 수정

--- src/hooks/useModal.tsx
+++ src/hooks/useModal.tsx
@@ -4,7 +4,7 @@ import { openModal, closeModal } from "../redux/modules/modal";
 function useModal() {
   const dispatch = useDispatch();
 
-  const handleOpenModal = (modalType: string, props: any) => {
+  const handleOpenModal = (modalType: string, props?: any) => {
     dispatch(openModal(modalType, props));
   };
 
