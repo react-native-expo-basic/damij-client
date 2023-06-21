7f3f89a3ea04700e72d2ee9186eb8ac80ce511bc
minseung-gang <strause1@nate.com>
2023-06-21 22:57:52 +0900

    Update: 내부 코드 리펙토링

--- src/components/likes/index.tsx
+++ src/components/likes/index.tsx
@@ -3,23 +3,29 @@ import { Image, View, Text, ActivityIndicator } from "react-native";
 import ImageContainer from "./ImageContainer";
 import styled from "styled-components/native";
 import FolderInfo from "./FolderInfo";
-import { ProductType } from "types/types";
-import { fetchProductLikeData } from "../../utils/productUtils";
+import { LikesFolderType } from "../../screens/Likes";
 
-interface Props {
-  cartItem: ProductType[];
+interface LikeFolderProps {
+  cartItem: LikesFolderType[];
 }
-
-export default function Index({ cartItem }: Props) {
+export default function Index({ cartItem }: LikeFolderProps) {
   return (
-    <Wrapper>
-      <ImageContainer products={cartItem} />
-      <FolderInfo product={cartItem} />
-    </Wrapper>
+    <>
+      {cartItem.map((item, idx) => (
+        <Wrapper
+          key={item.productInfo.folderName}
+          style={idx % 2 == 0 ? { marginRight: 7 } : { marginLeft: 7 }}
+        >
+          <ImageContainer images={item.image} />
+          <FolderInfo productInfo={item.productInfo} />
+        </Wrapper>
+      ))}
+    </>
   );
 }
 
 const Wrapper = styled.View`
-  width: 49%;
+  width: 50%;
   position: relative;
+  padding: 5px;
 `;
