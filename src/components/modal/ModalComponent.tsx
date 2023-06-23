7a957bca858186f52d080ad910df58c5fe467bdf
minseung-gang <strause1@nate.com>
2023-06-23 22:38:27 +0900

    Feat: 전역 모달을 여러개 띄우기 위해 모달리스트를 컴포넌트화

---
+++ src/components/modal/ModalComponent.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Modal, View } from "react-native";
+
+import LikeNotification from "./LikeNotification";
+import EditFolderModal from "./EditFolderModal";
+import LikeProductListModal from "./LikeProductListModal";
+import ConfirmModal from "./ConfirmModal";
+interface ModalComponentProps {
+  modalType: string;
+  props: any;
+}
+const ModalComponent: React.FC<ModalComponentProps> = ({
+  modalType,
+  props,
+}) => {
+  const MODAL_COMPONENTS: { [key: string]: JSX.Element } = {
+    alarm: <LikeNotification {...props} />,
+    editFolder: <EditFolderModal {...props} />,
+    likeDetail: <LikeProductListModal {...props} />,
+    confirm: <ConfirmModal {...props} />,
+  };
+
+  return <View>{MODAL_COMPONENTS[modalType]}</View>;
+};
+export default ModalComponent;
