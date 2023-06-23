a34df007b9a7be76943f6a0237058c1407c83cde
minseung-gang <strause1@nate.com>
2023-06-23 22:37:23 +0900

    Update:  상품 편집 및 삭제 기능 추가

--- src/components/modal/LikeProductListModal.tsx
+++ src/components/modal/LikeProductListModal.tsx
@@ -1,74 +1,162 @@
-import React, { useState } from "react";
-import { View, Text, TouchableOpacity, Modal, FlatList } from "react-native";
+import React, { useEffect, useState } from "react";
+import { TouchableOpacity, Modal, FlatList } from "react-native";
 import styled from "styled-components/native";
 import { SimpleLineIcons } from "@expo/vector-icons";
 import LikeProduct from "../../components/LikeProduct";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { AntDesign } from "@expo/vector-icons";
-import { textEditFolderColor } from "../../style";
-export interface ProductItem {
-  name: string;
-  image: string;
-  price: number;
-}
+import useModal from "../../hooks/useModal";
+import { fetchLikeProductData } from "../../utils/productUtils";
+import axios from "axios";
 
-export interface LikeProductProps {
+import {
+  textEditFolderColor,
+  textEditFolderFontSize,
+  textHeaderTitleSize,
+  textCountListColor,
+  textProductColor,
+  loadingSpinnerColor,
+} from "../../style";
+import LoadingSpinner from "../LoadingSpinner";
+
+interface LikeModalProps {
   folderName: string;
-  productItem: ProductItem[];
+}
+export interface ProductItem {
+  product_name: string;
+  product_price: number;
+  image: string;
+  discount_rate: number;
+  id: number;
 }
 
-export default function LikeProductListModal({
-  product,
-}: {
-  product: LikeProductProps;
-}) {
-  const [isVisible, setIsVisible] = useState(true);
-  const [isClicked, setIsClicked] = useState(false);
-  const renderItem = ({ item }: { item: ProductItem }) => (
-    <LikeProductContainer>
-      <LikeProduct {...item} />
-    </LikeProductContainer>
-  );
+export default function LikeProductListModal({ folderName }: LikeModalProps) {
+  const [isEditable, setIsEditable] = useState(false); // 편집 혹은 완료 버튼의 알림
+  const [selectedItems, setSelectedItems] = useState<number[]>([]); // 클릭한 좋아요 상품 아이디를 저장
+  const [productList, setProductList] = useState<ProductItem[]>([]); // 폴더 내에 있는 상품데이터들을 저장
+  const [isLoading, setIsLoading] = useState(false);
+  const { openModal, closeModal } = useModal();
+  const isClickable = selectedItems.length > 0;
+  // 폴더 내부에 있는 상품 데이터 가져오기
+  const handlerFolderData = async () => {
+    try {
+      const response = await fetchLikeProductData(folderName);
+      setIsLoading(false);
+      setProductList(response);
+    } catch (error) {
+      console.log(
+        "좋아요 상태의 상품을 가져오는 도중 오류가 발생했습니다.",
+        error
+      );
+    }
+  };
+
+  // 상품이 클릭 되어있는 지 체크하기
+  const handleCheckedItems = (id: number) => {
+    if (selectedItems.includes(id)) {
+      setSelectedItems(selectedItems.filter((itemId) => itemId !== id));
+    } else {
+      setSelectedItems([...selectedItems, id]);
+    }
+  };
+
+  // 편집 혹은 완료 버튼을 누를 때
+  const handleLikeProducts = () => {
+    if (!isEditable) {
+      setSelectedItems([]);
+      return setIsEditable(!isEditable);
+    }
+    setIsEditable(!isEditable);
+  };
+
+  //삭제 버튼을 클릭했을 때
+  const handleDeleteButton = () => {
+    openModal("confirm", {
+      message: "선택하신 상품을 삭제하시겠습니까?",
+      isOpen: true,
+      handler: async () => {
+        try {
+          await axios.post("http://192.168.35.55:3000/edit", {
+            selectedItems,
+          });
+        } catch (error) {
+          console.log("선택하신 상품을 삭제하는 과정에서 오류가 발생했습니다.");
+        }
+      },
+    });
+  };
+  useEffect(() => {
+    handlerFolderData();
+    setIsLoading(true);
+  }, []);
+
+  const renderItem = ({ item }: { item: ProductItem }) => {
+    return (
+      <LikeProduct
+        props={item}
+        selectedItems={selectedItems}
+        isEditable={isEditable}
+        toggleChecked={handleCheckedItems}
+      />
+    );
+  };
   return (
     <Modal
       animationType="slide"
-      onRequestClose={() => setIsVisible(!isVisible)}
-      visible={isVisible}
+      onRequestClose={() => closeModal("likeDetail")}
+      style={{ flex: 1 }}
     >
       <Header>
         <FlexContainer>
-          <SimpleLineIcons name="arrow-left" size={24} color="black" />
-          <Text>{product.folderName}</Text>
+          <SimpleLineIcons
+            name="arrow-left"
+            size={19}
+            color={textProductColor}
+            onPress={() => closeModal("likeDetail")}
+          />
+          <Title>{folderName}</Title>
         </FlexContainer>
-        <TouchableOpacity>
-          <EditButtonText>{isClicked ? "완료" : "편집"}</EditButtonText>
+        <TouchableOpacity onPress={handleLikeProducts}>
+          <EditButtonText>{isEditable ? "완료" : "편집"}</EditButtonText>
         </TouchableOpacity>
       </Header>
-      <FlatList
-        data={product.productItem}
-        keyExtractor={(item) => item.name}
-        renderItem={renderItem}
-        ListHeaderComponent={
-          <EditButtonText>
-            찜한 상품 수: {product.productItem.length}
-          </EditButtonText>
-        }
-      />
-      {isClicked ? (
-        <View>
-          <View>
+      <ProductContainer>
+        {isLoading ? (
+          <LoadingSpinner color={loadingSpinnerColor} />
+        ) : (
+          <FlatList
+            data={productList}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={renderItem}
+            numColumns={3}
+            ListHeaderComponent={
+              <LikeCountText>{`찜한 상품 ${productList.length}개`}</LikeCountText>
+            }
+          />
+        )}
+      </ProductContainer>
+      {isEditable ? (
+        <EditTabBar>
+          <EditButtonContainer disabled={!isClickable}>
             <MaterialCommunityIcons
               name="folder-move-outline"
               size={24}
-              color="black"
+              color={isClickable ? "black" : textCountListColor}
             />
-            <EditSecionText isClicked={isClicked}>폴더 이동</EditSecionText>
-          </View>
-          <View>
-            <AntDesign name="delete" size={24} color="black" />
-            <EditSecionText isClicked={isClicked}>삭제</EditSecionText>
-          </View>
-        </View>
+            <EditSecionText isClickable={isClickable}>폴더이동</EditSecionText>
+          </EditButtonContainer>
+          <EditButtonContainer
+            onPress={handleDeleteButton}
+            disabled={!isClickable}
+          >
+            <AntDesign
+              name="delete"
+              size={22}
+              color={isClickable ? "black" : textCountListColor}
+            />
+            <EditSecionText isClickable={isClickable}>삭제</EditSecionText>
+          </EditButtonContainer>
+        </EditTabBar>
       ) : (
         ""
       )}
@@ -79,19 +167,53 @@ export default function LikeProductListModal({
 const Header = styled.View`
   display: flex;
   flex-direction: row;
-  justify-content: flex-end;
+  justify-content: space-between;
+  padding: 7px 15px;
+  height: 50px;
+  align-items: center;
+`;
+const Title = styled.Text`
+  font-size: ${textHeaderTitleSize};
+  font-weight: 600;
+  margin-left: 20px;
 `;
 const FlexContainer = styled.View`
   display: flex;
   flex-direction: row;
+  align-items: center;
 `;
 const EditButtonText = styled.Text`
+  font-size: ${textEditFolderFontSize};
+  margin-right: 5px;
   color: ${textEditFolderColor};
 `;
-const LikeProductContainer = styled.View`
-  width: 100%;
-  margin-bottom: 10px;
+
+const EditSecionText = styled.Text<{ isClickable: boolean }>`
+  color: ${(props) => (props.isClickable ? "black" : textCountListColor)};
 `;
-const EditSecionText = styled.View<{ isClicked: boolean }>`
-  color: grey;
+const LikeCountText = styled.Text`
+  color: ${textCountListColor};
+  letter-spacing: -0.3px;
+  font-size: 15px;
+  margin-bottom: 6px;
+  font-weight: 500;
+  margin-left: 8px;
+`;
+const ProductContainer = styled.View`
+  flex: 1;
+  padding: 0 10px;
+  box-sizing: border-box;
+  margin-top: 20px;
+`;
+const EditTabBar = styled(FlexContainer)`
+  border-top-width: 0.5px;
+  border-color: #d5d5d5;
+
+  padding: 10px 0 8px;
+`;
+const EditButtonContainer = styled.TouchableOpacity`
+  width: 50%;
+
+  display: flex;
+  align-items: center;
 `;
