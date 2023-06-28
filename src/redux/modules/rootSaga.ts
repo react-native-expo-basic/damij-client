9d317ecab1648a5aa7cd03669313c6137cedfd4a
minseung-gang <strause1@nate.com>
2023-06-28 23:28:33 +0900

    Update: folder 리듀서에  있는 folderSaga추가

--- src/redux/modules/rootSaga.ts
+++ src/redux/modules/rootSaga.ts
@@ -1,5 +1,6 @@
 import { all } from "redux-saga/effects";
 import { callSaga } from "./likes";
+import { watchFolderSaga } from "./folder";
 export default function* rootSaga() {
-  yield all([callSaga()]); // 하위 사가들을 배열로 넣음
+  yield all([callSaga(), watchFolderSaga()]); // 하위 사가들을 배열로 넣음
 }
