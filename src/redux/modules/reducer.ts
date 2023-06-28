25dda85306c0596faa6da7b24ece5250238c994a
minseung-gang <strause1@nate.com>
2023-06-28 23:34:41 +0900

    Update: folder 리듀서추가

--- src/redux/modules/reducer.ts
+++ src/redux/modules/reducer.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import likes from "./likes";
 import modalState from "./modal";
+import folder from "./folder";
 const reducer = combineReducers({
   likes,
   modal: modalState,
+  folder,
 });
 
 export default reducer;
