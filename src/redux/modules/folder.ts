1fb09418656fd82fb4a12dc8e343f8b68503a0e5
minseung-gang <strause1@nate.com>
2023-07-04 19:59:22 +0900

    update: 폴더모달 타입 수정

--- src/redux/modules/folder.ts
+++ src/redux/modules/folder.ts
@@ -1,9 +1,9 @@
 import { Action } from "redux-actions";
 import { put, call, select, takeEvery } from "redux-saga/effects";
-import { openModal } from "./modal";
-
+import { openModal, closeModal } from "./modal";
+import { ProductItem } from "../../components/modal/LikeProductListModal";
 interface FoldersType {
-  products: number[];
+  productsId: number[];
   changeFolder: string;
 }
 interface FolderSagaAction {
@@ -12,6 +12,7 @@ interface FolderSagaAction {
 }
 interface ModalStateType extends FoldersType {
   originFolder: string;
+  products: ProductItem[];
 }
 const prefix = "http://192.168.35.71:3000";
 
@@ -23,18 +24,18 @@ const SUCCESS = `${prefix}/SUCCESS`;
 const FAIL = `${prefix}/FAIL`;
 
 // 액션 생성자 함수 정의
-export const openFolder = (originFolder: string) => ({
+export const openFolder = (originFolder: string, products: ProductItem[]) => ({
   type: FOLDER_NAME,
-  payload: { originFolder },
+  payload: { originFolder, products },
 });
-export const changeFolder = (products: FoldersType) => ({
+export const changeFolder = (info: FoldersType) => ({
   type: FOLDER,
-  payload: products,
+  payload: info,
 });
 const pending = () => ({ type: PENDING });
-const success = (folders: FoldersType) => ({
+const success = (productsId: number[]) => ({
   type: SUCCESS,
-  payload: folders,
+  payload: productsId,
 });
 
 const fail = (error: Error | null) => ({ type: FAIL, payload: error });
@@ -42,6 +43,8 @@ const initialState = {
   changeFolder: "",
   originFolder: "",
   products: [],
+  productsId: [],
+  filteredProducts: [],
   loading: false,
   error: null,
 };
@@ -56,13 +59,13 @@ const folderState = (state = initialState, action: Action<ModalStateType>) => {
       return {
         ...state,
         originFolder: action.payload.originFolder,
+        products: action.payload.products,
       };
-
+    // success > 모달 내부의 상품리스트들로 수정 예정
     case SUCCESS:
       return {
         ...state,
-        changeFolder: action.payload.changeFolder,
-        products: action.payload.products,
+        filteredProducts: action.payload,
       };
     case FAIL:
       return { ...state, loading: false, error: action.payload };
@@ -77,23 +80,23 @@ function* folderSaga(action: FolderSagaAction) {
     const { originFolder } = yield select((state) => state.folder);
 
     if (action.payload.changeFolder === originFolder) {
-      console.log("같음");
+      // alert 모달 추가 후 수정 예정
+      yield put(closeModal("likeDetail"));
       yield put(
-        openModal("confirm", {
-          message: "중복되는 애들이에요",
-          handler: () => {
-            console.log("ddd");
-          },
+        openModal("alert", {
+          message: "같은 폴더로는 상품을 옮길 수 없습니다.",
         })
       );
       return;
     }
-    yield put(
-      success({
-        changeFolder: action.payload.changeFolder,
-        products: action.payload.products,
-      })
+    // 서버 연동 할 함수 추가 예정
+    const { products } = yield select((state) => state.folder);
+
+    const filterItems = products.filter(
+      (product: ProductItem) => !action.payload.productsId.includes(product.id)
     );
+    console.log("필터링된 아이템", filterItems);
+    yield put(success(filterItems));
   } catch (error: any) {
     yield put(fail(error));
   }
