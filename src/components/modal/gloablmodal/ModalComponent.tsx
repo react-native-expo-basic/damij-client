97da107726fac09448d64a5a5605fa618bbee9f2
minseung-gang <strause1@nate.com>
2023-07-04 20:00:42 +0900

    update: alert modal 추가

--- src/components/modal/gloablmodal/ModalComponent.tsx
+++ src/components/modal/gloablmodal/ModalComponent.tsx
@@ -5,6 +5,7 @@ import EditFolderModal from "../EditFolderModal";
 import LikeProductListModal from "../LikeProductListModal";
 import ConfirmModal from "../ConfirmModal";
 import FolderSelectionModal from "../selectionModal/FolderSelectionModal";
+import AlertModal from "../AlertModal";
 
 interface ModalComponentProps {
   modalType: string;
@@ -21,6 +22,7 @@ const ModalComponent: React.FC<ModalComponentProps> = ({
     likeDetail: <LikeProductListModal {...props} />,
     confirm: <ConfirmModal {...props} />,
     handleFolder: <FolderSelectionModal {...props} />,
+    alert: <AlertModal {...props} />,
   };
 
   return <View>{MODAL_COMPONENTS[modalType]}</View>;
