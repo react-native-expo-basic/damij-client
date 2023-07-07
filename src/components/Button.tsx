4551065eb279213533c4c2343035437ff9688e9d
minseung-gang <strause1@nate.com>
2023-07-07 23:54:44 +0900

    Update: 버튼 컴포넌트 코드 수정

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,47 +5,73 @@ import styled from "styled-components/native";
 import { textDisableColor, viewDisableColor } from "../style";
 
 interface FilterButtonProps extends ButtonType {
+  color?: string;
   isSelected?: boolean;
 }
 
 const width = Dimensions.get("window").width / 5 - 7;
 
-export const BasicButton: React.FC<ButtonType> = ({ texts, event }) => {
-  return (
-    <TouchableOpacity onPress={event} accessibilityLabel={texts}>
-      <Text>{texts}</Text>
-    </TouchableOpacity>
-  );
-};
-
-export const FilterButton: React.FC<FilterButtonProps> = ({
-  texts,
-  event,
-  isSelected,
+export const BasicButton: React.FC<ButtonType> = ({
+  children,
+  onPress,
+  color = "black",
+  background = "white",
 }) => {
   return (
     <ButtonContainer
-      onPress={event}
-      accessibilityLabel={texts}
-      isSelected={isSelected}
-      style={{ width: width }}
+      onPress={onPress}
+      accessibilityLabel={children}
+      background={background}
     >
-      <ButtonText style={{ color: isSelected ? "#444444" : textDisableColor }}>
-        {texts}
-      </ButtonText>
+      <BasicButtonText color={color}>{children}</BasicButtonText>
     </ButtonContainer>
   );
 };
 
-const ButtonContainer = styled.TouchableOpacity<{ isSelected?: boolean }>`
+export const FilterButton: React.FC<FilterButtonProps> = ({
+  children,
+  onPress,
+  background = "white",
+  isSelected = false,
+}) => {
+  return (
+    <FilterContainer
+      onPress={onPress}
+      accessibilityLabel={children}
+      isSelected={isSelected}
+      background={background}
+      width={width}
+    >
+      <FilterButtonText isSelected={isSelected}>{children}</FilterButtonText>
+    </FilterContainer>
+  );
+};
+
+const ButtonContainer = styled.TouchableOpacity<{ background?: string }>`
+  border-radius: 5px;
+  padding: 5px 10px;
+  background: ${(props) => props.background};
+  display: flex;
+  justify-content: center;
+`;
+const BasicButtonText = styled.Text<{ color?: string }>`
+  color: ${(props) => props.color};
+`;
+const FilterContainer = styled.TouchableOpacity<{
+  isSelected: boolean;
+  background?: string;
+  width: number;
+}>`
   border-radius: 5px;
   padding: 10px 15px;
-  height: 100%;
   display: flex;
   justify-content: center;
   background: ${(props) => (props.isSelected ? "#fff" : viewDisableColor)};
+  width: ${(props) => props.width}px;
 `;
-const ButtonText = styled.Text<{ isSelected?: boolean }>`
+
+const FilterButtonText = styled.Text<{ isSelected?: boolean }>`
   text-align: center;
+  color: ${(props) => (props.isSelected ? "#444444" : textDisableColor)};
   font-weight: 600;
 `;
