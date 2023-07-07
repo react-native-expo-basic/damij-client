b18ebac4b83dbd5511edafbb85979173836fe7e8
minseung-gang <strause1@nate.com>
2023-07-07 23:55:33 +0900

    feat: 마이페이지 추가중

--- src/screens/Mypage.tsx
+++ src/screens/Mypage.tsx
@@ -1,10 +1,44 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, TouchableOpacity, Alert } from "react-native";
+import useModal from "../hooks/useModal";
+import { AuthStateType, logout } from "../redux/modules/auth";
+import { useDispatch, useSelector } from "react-redux";
+
+interface authState {
+  auth: AuthStateType;
+}
 
 export default function Mypage() {
+  const { openModal } = useModal();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const auth = useSelector((state: authState) => state.auth);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    setIsLoggedIn(auth.isLogin);
+  }, [auth]);
+
+  const handleLogout = async () => {
+    dispatch(logout());
+  };
   return (
     <View>
-      <Text>Mypage</Text>
+      {isLoggedIn ? (
+        <View>
+          <TouchableOpacity onPress={() => handleLogout()}>
+            <Text>로그아웃</Text>
+          </TouchableOpacity>
+        </View>
+      ) : (
+        <View>
+          <TouchableOpacity onPress={() => openModal("signIn")}>
+            <Text>로그인</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => openModal("signUp")}>
+            <Text>회원가입</Text>
+          </TouchableOpacity>
+        </View>
+      )}
     </View>
   );
 }
