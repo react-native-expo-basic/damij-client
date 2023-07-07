7e166e88e058b0a0b8e22a89da7b907f96581ef0
minseung-gang <strause1@nate.com>
2023-07-07 23:56:17 +0900

    Update: 토큰 저장 및 삭제 기능 업데이터

--- src/services/TokenSerivce.ts
+++ src/services/TokenSerivce.ts
@@ -1,12 +1,32 @@
-const LOCAL_STORAGE_KEY_NAME = "token";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 export default class TokenService {
-  public static get(): string | null {
-    return localStorage.getItem(LOCAL_STORAGE_KEY_NAME);
+  static token: string | null = null;
+
+  static async get(): Promise<string | null> {
+    try {
+      const token = await AsyncStorage.getItem("token");
+      return token;
+    } catch (error) {
+      console.error("토큰을 가져오는 도중에 오류가 발생했습니다.", error);
+      return null;
+    }
   }
-  public static set(token: string): void {
-    return localStorage.setItem(LOCAL_STORAGE_KEY_NAME, token);
+
+  static async set(token: string): Promise<void> {
+    try {
+      await AsyncStorage.setItem("token", token);
+    } catch (error) {
+      console.error("토큰을 저장하는 도중에 오류가 발생했습니다.", error);
+    }
   }
-  public static remove(): void {
-    return localStorage.removeItem(LOCAL_STORAGE_KEY_NAME);
+
+  static async remove(): Promise<void> {
+    try {
+      await AsyncStorage.removeItem("token");
+      console.log("토큰 삭제");
+    } catch (error) {
+      console.error("토큰을 삭제하는 도중에 오류가 발생했습니다.", error);
+    }
   }
 }
