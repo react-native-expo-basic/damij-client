e5b9a74b319206d23e29270b04375e507a1e9cea
minseung-gang <strause1@nate.com>
2023-07-08 23:47:08 +0900

    Update: 토큰 유무 확인해서 api 사용하도록 코드 수정

--- src/api/productApi.ts
+++ src/api/productApi.ts
@@ -1,13 +1,51 @@
-import instance from "./api";
 import axios from "axios";
-
+import { LikesProductType } from "../types/types";
+import { authInstance, instance } from "./api";
+import TokenService from "../services/TokenSerivce";
 export async function fetchProductData(category: string) {
   try {
-    const response = await axios.get(
-      `http://13.124.181.25/api/product/listnouser?keyword=${category}`
+    const isToken = TokenService.get();
+    console.log(isToken);
+    if (isToken == null) {
+      const response = await instance.get(
+        `api/product/list?keyword=${category}`
+      );
+      return response.data;
+    }
+    const response = await authInstance.get(
+      `api/product/list?keyword=${category}`
     );
     return response.data;
   } catch (error) {
     console.log(error);
+  }
+}
+export async function filtetedProductData(category: string) {
+  try {
+    const response = await authInstance.get(
+      `api/product/list?category=${category}`
+    );
+
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function updateProductLikedStatus({
+  productId,
+  isLiked,
+}: LikesProductType) {
+  try {
+    const response = await authInstance.post(`api/my/pickproduct`, {
+      productId,
+    });
+    console.log("좋아요 리스폰스", response.data);
+    return response.data;
+  } catch (error) {
+    console.error(
+      "상품의 좋아요 상태를 업데이트하는 도중 오류가 발생했습니다.",
+      error
+    );
   }
 }
