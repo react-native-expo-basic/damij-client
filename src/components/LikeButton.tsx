1e6fd63a1ca96c2f2a141073023e1f63aa2f1699
minseung-gang <strause1@nate.com>
2023-07-08 23:42:08 +0900

    Update: 토큰 유무 확인 후 모달 실행시키는 코드 추가

--- src/components/LikeButton.tsx
+++ src/components/LikeButton.tsx
@@ -1,28 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components/native";
 import { Octicons } from "@expo/vector-icons";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { toggleLike } from "../redux/modules/likes";
 import useModal from "../hooks/useModal";
+import { uploadProductLikeData } from "../utils/productUtils";
+import { ProductType } from "../types/types";
+import { LikesProductType } from "../types/types";
+import TokenService from "../services/TokenSerivce";
 
 interface LikesBtnProps {
-  isLiked: boolean;
+  isSelected: boolean;
   productId: number;
 }
 
-const LikeButton: React.FC<LikesBtnProps> = ({ isLiked, productId }) => {
+const LikeButton: React.FC<LikesBtnProps> = ({ isSelected, productId }) => {
   const { openModal } = useModal();
   const dispatch = useDispatch();
+  const [isLiked, setIsSelected] = useState(isSelected);
+  const toggleProductLikedStatus = async () => {
+    const isToken = await TokenService.get();
 
-  const toggleProductLikedStatus = () => {
-    dispatch(toggleLike({ productId, isLiked }));
-    openModal("alarm", { isLiked, productId });
+    if (isToken !== null) {
+      dispatch(toggleLike({ productId, isLiked }));
+      openModal("alarm", { isLiked, productId });
+      return;
+    }
+
+    openModal("alert", {
+      message: "좋아요 버튼은 로그인을 하셔야 사용가능합니다.",
+    });
   };
 
   return (
     <LikesBtn
-      name={isLiked ? "heart-fill" : "heart"}
-      isLiked={isLiked}
+      name={isSelected ? "heart-fill" : "heart"}
+      isSelected={isLiked}
       size={24}
       onPress={toggleProductLikedStatus}
     />
@@ -31,7 +44,7 @@ const LikeButton: React.FC<LikesBtnProps> = ({ isLiked, productId }) => {
 
 export default LikeButton;
 
-const LikesBtn = styled(Octicons)<LikesBtnProps>`
+const LikesBtn = styled(Octicons)<{ isLiked: boolean }>`
   position: absolute;
   bottom: 8px;
   right: 8px;
