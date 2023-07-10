d34c5d7cc72acb35891d44ed46cc7c7658b2d6c1
minseung-gang <strause1@nate.com>
2023-07-11 00:01:02 +0900

    Update: 토큰 관련 리듀서 추가

--- src/redux/modules/rootSaga.ts
+++ src/redux/modules/rootSaga.ts
@@ -1,6 +1,7 @@
 import { all } from "redux-saga/effects";
 import { callSaga } from "./likes";
 import { watchFolderSaga } from "./folder";
+import { authSaga } from "./auth";
 export default function* rootSaga() {
-  yield all([callSaga(), watchFolderSaga()]); // 하위 사가들을 배열로 넣음
+  yield all([callSaga(), watchFolderSaga(), authSaga()]); // 하위 사가들을 배열로 넣음
 }
