577d14292254269ea2145969efd6be5d10c66bbb
minseung-gang <strause1@nate.com>
2023-07-10 23:55:16 +0900

    Update: 로그인 및 비로그인 좋아요 데이터 분류하도록 코드 수정

--- src/components/home/Main/ProductList.tsx
+++ src/components/home/Main/ProductList.tsx
@@ -1,33 +1,46 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import styled from "styled-components/native";
 import Product from "../../Product";
 import { ProductType } from "../../../types/types";
 import { fetchProductData } from "../../../api/productApi";
-export interface MainProps {
-  newProduct: ProductType[];
-  bestProduct: ProductType[];
-}
+import LoadingSpinner from "../../../components/LoadingSpinner";
+import { useSelector } from "react-redux";
+import { MainLikesState, MainAuthState } from "../../../types/types";
 
 export default function ProductList() {
   const [fetchData, setFetchData] = useState({
     new: { NewProduct: [] },
     best: { BestProduct: [] },
   });
-  const fetchItems = async () => {
+
+  const [loading, setLoading] = useState(true);
+
+  const likesState = useSelector((state: MainLikesState) => state.likes.likes);
+
+  const authState = useSelector((state: MainAuthState) => state.auth);
+
+  const fetchItems = useCallback(async () => {
     try {
-      const [newProducts, popularProducts] = await Promise.all([
-        fetchProductData("NewProduct"),
-        fetchProductData("BestProduct"),
-      ]);
+      const newProducts = await fetchProductData("NewProduct");
+      const popularProducts = await fetchProductData("BestProduct");
+
       setFetchData({ new: newProducts, best: popularProducts });
+      setLoading(false);
     } catch (error) {
       console.log(error);
     }
-  };
+  }, []);
 
   useEffect(() => {
     fetchItems();
-  }, []);
+  }, [authState]);
+  // 좋아요 버튼을 눌렀을 떄 좋아요 상태 최신화
+  // 로그인 했을때 토큰 값이 존재할때 상품 좋아요 상태 최신화
+
+  if (loading) {
+    // 로딩 중일 때 표시할 내용
+    return <LoadingSpinner />;
+  }
 
   return (
     <PaddingView>
@@ -46,16 +59,12 @@ export default function ProductList() {
     </PaddingView>
   );
 }
-
 const PaddingView = styled.View`
   padding: 15px 15px 0;
   box-sizing: border-box;
 `;
 
-const TitleContainer = styled.View`
-  display: flex;
-  flex-direction: row;
-`;
+const TitleContainer = styled.View``;
 
 const TitleText = styled.Text`
   font-size: 20px;
