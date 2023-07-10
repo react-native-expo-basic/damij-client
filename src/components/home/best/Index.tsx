1dc6847a299b8c95f681eca891d432d43b10b2fe
minseung-gang <strause1@nate.com>
2023-07-10 23:56:13 +0900

    Update: 상품 필터링하는 코드 api에 맞게 수정

--- src/components/home/best/Index.tsx
+++ src/components/home/best/Index.tsx
@@ -1,29 +1,42 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import Category from "../../Category";
 import { fetchProductData } from "../../../api/productApi";
 import { MainProps, ProductType } from "../../../types/types";
 import Product from "../../Product";
 import styled from "styled-components/native";
 import { FlatList, View } from "react-native";
-import { DataType } from "../../../types/types";
+import { MainLikesState, MainAuthState } from "../../../types/types";
+import { useSelector } from "react-redux";
+import { LikesState } from "../../../types/types";
+import { filtetedProductData } from "../../../api/productApi";
 
 export default function Index() {
   const data = [{ id: "category" }, { id: "product" }];
-
   const [bestItems, setBestItems] = useState<ProductType[]>([]);
-  console.log(bestItems);
-  const fetchItems = async () => {
+  const likesState = useSelector((state: MainLikesState) => state.likes);
+  const authState = useSelector((state: MainAuthState) => state.auth);
+  const fetchItems = useCallback(async () => {
     try {
       const popularProducts = await fetchProductData("BestProduct");
       setBestItems(popularProducts.BestProduct);
     } catch (error) {
       console.log(error);
     }
-  };
+  }, []);
+
+  const filteredItems = useCallback(async (category: string) => {
+    try {
+      const filteredItem = await filtetedProductData(category);
+
+      setBestItems(filteredItem[category]);
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
 
   useEffect(() => {
     fetchItems();
-  }, []);
+  }, [authState]);
 
   const renderItem = ({ item }: { item: ProductType }) => {
     return <Product products={[item]} />;
@@ -34,7 +47,9 @@ export default function Index() {
         data={bestItems}
         renderItem={renderItem}
         keyExtractor={(item) => item.id.toString()}
-        ListHeaderComponent={<Category />}
+        ListHeaderComponent={
+          <Category filteredItems={filteredItems} fetchItems={fetchItems} />
+        }
         numColumns={2}
       />
     </PaddingView>
