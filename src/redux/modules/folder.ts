c1b5f4b7f1b320c25969053ef5d98610a909c105
minseung-gang <strause1@nate.com>
2023-07-10 23:52:52 +0900

    Update: 폴더 리스트 상태관리하는 코드 수정

--- src/redux/modules/folder.ts
+++ src/redux/modules/folder.ts
@@ -2,10 +2,14 @@ import { Action } from "redux-actions";
 import { put, call, select, takeEvery } from "redux-saga/effects";
 import { openModal, closeModal } from "./modal";
 import { ProductItem } from "../../components/modal/LikeProductListModal";
+import { AddFolder } from "../../api/folderApi";
+import { LikesFolderType } from "../../screens/Likes";
+
 interface FoldersType {
   productsId: number[];
   changeFolder: string;
 }
+
 interface FolderSagaAction {
   type: string;
   payload: FoldersType;
@@ -13,19 +17,38 @@ interface FolderSagaAction {
 interface ModalStateType extends FoldersType {
   originFolder: string;
   products: ProductItem[];
+  productList: LikesFolderType[];
+}
+
+interface AddFolderType {
+  type: string;
+  payload: { inputValue: string };
 }
 const prefix = "http://192.168.35.71:3000";
 
 // 액션 타입 정의
 const FOLDER = `${prefix}/FOLDER`;
 const FOLDER_NAME = `${prefix}/FOLDER_NAME`;
+const DELETE_FOLDER = `${prefix}/DELETE_FOLDER`;
 const PENDING = `${prefix}/PENDING`;
-const SUCCESS = `${prefix}/SUCCESS`;
+const SUCCESS_ADD = `${prefix}/SUCCESS_ADD`;
 const FAIL = `${prefix}/FAIL`;
+const ADD_FOLDER = `${prefix}/ADD_FOLDER`;
+const ADD_FOLDER_LIST = `${prefix}/ADD_FOLDER_LIST`;
 
 // 액션 생성자 함수 정의
+
+export const addFolder = (inputValue: string) => ({
+  type: ADD_FOLDER,
+  payload: { inputValue },
+});
+
+export const addFolderList = (productList: LikesFolderType[]) => ({
+  type: ADD_FOLDER_LIST,
+  payload: productList,
+});
 export const openFolder = (originFolder: string, products: ProductItem[]) => ({
-  type: FOLDER_NAME,
+  type: DELETE_FOLDER,
   payload: { originFolder, products },
 });
 export const changeFolder = (info: FoldersType) => ({
@@ -33,15 +56,18 @@ export const changeFolder = (info: FoldersType) => ({
   payload: info,
 });
 const pending = () => ({ type: PENDING });
-const success = (productsId: number[]) => ({
-  type: SUCCESS,
+const success_deleteProducts = (productsId: number[]) => ({
+  type: DELETE_FOLDER,
   payload: productsId,
 });
-
+const success_addFolder = (inputValue: string) => ({
+  type: SUCCESS_ADD,
+  payload: inputValue,
+});
 const fail = (error: Error | null) => ({ type: FAIL, payload: error });
 const initialState = {
   changeFolder: "",
-  originFolder: "",
+  originFolder: [],
   products: [],
   productsId: [],
   filteredProducts: [],
@@ -54,15 +80,31 @@ const folderState = (state = initialState, action: Action<ModalStateType>) => {
   switch (action.type) {
     case PENDING:
       return { ...state, loading: true };
-
+    case ADD_FOLDER:
+      return {
+        ...state,
+        originFolder: [
+          ...state.originFolder,
+          { name: action.payload, folderCount: 0, imgList: [] },
+        ],
+      };
+    case ADD_FOLDER_LIST:
+      return { ...state, originFolder: action.payload };
+    case SUCCESS_ADD:
+      return {
+        ...state,
+        originFolder: [
+          ...state.originFolder,
+          { name: action.payload, folderCount: 0, imgList: [] },
+        ],
+      };
     case FOLDER_NAME:
       return {
         ...state,
         originFolder: action.payload.originFolder,
         products: action.payload.products,
       };
-    // success > 모달 내부의 상품리스트들로 수정 예정
-    case SUCCESS:
+    case DELETE_FOLDER:
       return {
         ...state,
         filteredProducts: action.payload,
@@ -73,8 +115,7 @@ const folderState = (state = initialState, action: Action<ModalStateType>) => {
       return state;
   }
 };
-
-function* folderSaga(action: FolderSagaAction) {
+function* filterProductSaga(action: FolderSagaAction) {
   try {
     yield put(pending());
     const { originFolder } = yield select((state) => state.folder);
@@ -95,14 +136,23 @@ function* folderSaga(action: FolderSagaAction) {
     const filterItems = products.filter(
       (product: ProductItem) => !action.payload.productsId.includes(product.id)
     );
-    console.log("필터링된 아이템", filterItems);
-    yield put(success(filterItems));
+    yield put(success_deleteProducts(filterItems));
   } catch (error: any) {
     yield put(fail(error));
   }
 }
-
+function* addFolderSaga(action: AddFolderType) {
+  try {
+    yield put(pending());
+    yield call(AddFolder, action.payload.inputValue);
+    yield put(success_addFolder(action.payload.inputValue));
+    yield put(closeModal("editFolder"));
+  } catch (error: any) {
+    yield put(fail(error));
+  }
+}
 export function* watchFolderSaga() {
-  yield takeEvery(FOLDER, folderSaga);
+  yield takeEvery(DELETE_FOLDER, filterProductSaga);
+  yield takeEvery(ADD_FOLDER, addFolderSaga);
 }
 export default folderState;
