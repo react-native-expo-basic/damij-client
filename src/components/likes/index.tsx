aae836989de7f7281704f8bf4c95e23aba8c578c
minseung-gang <strause1@nate.com>
2023-07-13 00:01:53 +0900

    Refector: 찜 카테고리 영역 코드 리펙토링

--- src/components/likes/index.tsx
+++ src/components/likes/index.tsx
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from "react";
-import { TouchableOpacity } from "react-native";
 import ImageContainer from "./ImageContainer";
 import styled from "styled-components/native";
 import FolderInfo from "./FolderInfo";
-import { fetchLikeProductData } from "../../utils/productUtils";
-import { LikesFolderType } from "../../screens/Likes";
 import useModal from "../../hooks/useModal";
+import { LikesFolderType } from "../../screens/Likes";
+
 interface LikeFolderProps {
-  cartItem: LikesFolderType[];
+  cartItem: LikesFolderType;
+  index: number;
 }
-export default function Index({ cartItem }: LikeFolderProps) {
+export default function Index({ cartItem, index }: LikeFolderProps) {
   const { openModal } = useModal();
+  const { folderCount, imgList, name } = cartItem;
+  const productInfo = { folderCount, name };
   const handleLikeDetailModal = async (folderName: string) => {
     try {
       openModal("likeDetail", { folderName });
@@ -19,25 +21,19 @@ export default function Index({ cartItem }: LikeFolderProps) {
     }
   };
   return (
-    <>
-      {cartItem.map((item, idx) => {
-        return (
-          <Wrapper
-            key={item.productInfo.folderName}
-            onPress={() => handleLikeDetailModal(item.productInfo.folderName)}
-            style={idx % 2 == 0 ? { marginRight: 7 } : { marginLeft: 7 }}
-          >
-            <ImageContainer images={item.image} />
-            <FolderInfo productInfo={item.productInfo} />
-          </Wrapper>
-        );
-      })}
-    </>
+    <Wrapper
+      onPress={() => handleLikeDetailModal(name)}
+      style={index % 2 == 0 ? { marginRight: 7 } : { marginLeft: 7 }}
+    >
+      <ImageContainer images={imgList} />
+      <FolderInfo productInfo={productInfo} />
+    </Wrapper>
   );
 }
 
 const Wrapper = styled.TouchableOpacity`
-  width: 50%;
+  width: 48%;
   position: relative;
   padding: 5px;
+  margin-bottom: 10px;
 `;
