7bc777f1b7730b279856d7c1edd9c0baf7267ecc
minseung-gang <strause1@nate.com>
2023-07-13 00:14:52 +0900

    Add: 폴더 삭제, 폴더 내부 상품 삭제 관련 api추가

--- src/api/folderApi.ts
+++ src/api/folderApi.ts
@@ -1,18 +1,48 @@
 import axios from "axios";
 import { authInstance } from "./api";
-
+import useModal from "../hooks/useModal";
 export async function AddFolder(inputValue: string) {
   try {
-    console.log(inputValue, "텍스트");
     const response = await authInstance.post(
       `/api/my/createfolder/${inputValue}`,
       {
         name: inputValue,
       }
     );
-    console.log(response.data, "api 결과");
+
     return response.data;
   } catch (error) {
     console.log(error);
   }
 }
+
+export const handleDeleteFolder = async (folderName: string) => {
+  try {
+    const response = await authInstance.delete(
+      `/api/my/deletefolder/${folderName}`
+    );
+  } catch (error) {
+    console.log("폴더를 삭제하는 도중 오류가 발생했습니다.", error);
+  }
+};
+
+export const handleDeleteProducts = async (
+  choiceName: string,
+  productIdList: number[]
+) => {
+  try {
+    console.log(
+      {
+        choiceName,
+        productIdList,
+      },
+      "상품 삭제 함수"
+    );
+    await authInstance.post(`/api/my/deletepick`, {
+      choiceName,
+      productIdList,
+    });
+  } catch (error) {
+    console.log("폴더 상품들을 삭제하는 도중 오류가 발생했습니다.", error);
+  }
+};
