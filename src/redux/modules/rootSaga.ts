616fe0a621698e0e723c954f5f80508ec89808c6
minseung-gang <strause1@nate.com>
2023-07-13 14:07:22 +0900

    Refector: 폴더사가 경로 수정

--- src/redux/modules/rootSaga.ts
+++ src/redux/modules/rootSaga.ts
@@ -1,6 +1,6 @@
 import { all } from "redux-saga/effects";
 import { callSaga } from "./likes";
-import { watchFolderSaga } from "./folder";
+import { watchFolderSaga } from "./folderSaga";
 import { authSaga } from "./auth";
 export default function* rootSaga() {
   yield all([callSaga(), watchFolderSaga(), authSaga()]); // 하위 사가들을 배열로 넣음
