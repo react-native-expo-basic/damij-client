e7d79ceb6297d66a447bd64c699739ae753712e1
minseung-gang <strause1@nate.com>
2023-07-15 01:18:35 +0900

    FIx: 토큰을 저장할 때 유저와 게스트를 분류하여 저장하도록 수정

--- src/services/TokenSerivce.ts
+++ src/services/TokenSerivce.ts
@@ -1,9 +1,11 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default class TokenService {
-  static async get(): Promise<string | null> {
+  static async getToken(tokenType: "user" | "guest"): Promise<string | null> {
     try {
-      const token = await AsyncStorage.getItem("token");
+      const token = await AsyncStorage.getItem(
+        tokenType === "user" ? "userToken" : "guestToken"
+      );
       return token;
     } catch (error) {
       console.error("토큰을 가져오는 도중에 오류가 발생했습니다.", error);
@@ -11,17 +13,25 @@ export default class TokenService {
     }
   }
 
-  static async set(token: string): Promise<void> {
+  static async setToken(
+    tokenType: "user" | "guest",
+    token: string
+  ): Promise<void> {
     try {
-      await AsyncStorage.setItem("token", token);
+      await AsyncStorage.setItem(
+        tokenType === "user" ? "userToken" : "guestToken",
+        token
+      );
     } catch (error) {
       console.error("토큰을 저장하는 도중에 오류가 발생했습니다.", error);
     }
   }
 
-  static async remove(): Promise<void> {
+  static async removeToken(tokenType: "user" | "guest"): Promise<void> {
     try {
-      await AsyncStorage.removeItem("token");
+      await AsyncStorage.removeItem(
+        tokenType === "user" ? "userToken" : "guestToken"
+      );
     } catch (error) {
       console.error("토큰을 삭제하는 도중에 오류가 발생했습니다.", error);
     }
