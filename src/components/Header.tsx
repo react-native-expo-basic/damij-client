7719fc5f1b110c918cd3671838caac02ade30ffc
minseung-gang <strause1@nate.com>
2023-07-14 23:56:47 +0900

    chore: 필요없는 코드 삭제

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { View, Text, Image } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { Feather } from "@expo/vector-icons";
-import { SimpleLineIcons } from "@expo/vector-icons";
 import styled from "styled-components/native";
 
 export default function Header() {
