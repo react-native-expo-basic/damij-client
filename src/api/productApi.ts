2dd27c2df12e0c7e5e55c1f7e08c0e6daa8f1491
minseung-gang <strause1@nate.com>
2023-07-14 23:39:32 +0900

    chore: 필요없는 코드 삭제

--- src/api/productApi.ts
+++ src/api/productApi.ts
@@ -1,17 +1,10 @@
 import axios from "axios";
 import { LikesProductType } from "../types/types";
-import { authInstance, instance } from "./api";
+import { authInstance } from "./api";
 import TokenService from "../services/TokenSerivce";
+
 export async function fetchProductData(category: string) {
   try {
-    const isToken = TokenService.get();
-    console.log(isToken);
-    if (isToken == null) {
-      const response = await instance.get(
-        `api/product/list?keyword=${category}`
-      );
-      return response.data;
-    }
     const response = await authInstance.get(
       `api/product/list?keyword=${category}`
     );
@@ -32,6 +25,7 @@ export async function filtetedProductData(category: string) {
   }
 }
 
+//좋아요 버튼 클릭
 export async function updateProductLikedStatus({
   productId,
   isLiked,
@@ -40,12 +34,31 @@ export async function updateProductLikedStatus({
     const response = await authInstance.post(`api/my/pickproduct`, {
       productId,
     });
-    console.log("좋아요 리스폰스", response.data);
+
     return response.data;
   } catch (error) {
     console.error(
       "상품의 좋아요 상태를 업데이트하는 도중 오류가 발생했습니다.",
       error
     );
+  }
+}
+
+// 찜 카테고리에 저장된 폴더 데이터 가져오는 Api
+export async function fetchLikeFolderData() {
+  try {
+    const response = await authInstance.get(`/api/my/readFolder`);
+    return response.data;
+  } catch (error) {
+    console.error("좋아요 폴더를 가져오는 도중 오류가 발생했습니다.");
+  }
+}
+
+export async function fetchLikeProductData(folderName: string) {
+  try {
+    const response = await authInstance.get(`/api/my/folderInfo/${folderName}`);
+    return response.data;
+  } catch (error) {
+    console.error("좋아요 폴더의 상품을 가져오는 도중 오류가 발생했습니다.");
   }
 }
