3f137ae3f2008cf8ac08daaf643eca3d53e9f0fd
minseung-gang <strause1@nate.com>
2023-07-14 18:16:27 +0900

    Feat: 폴더이동 관련 사가 추가

--- src/redux/modules/folderSaga.ts
+++ src/redux/modules/folderSaga.ts
@@ -2,7 +2,11 @@ import { put, call, select, takeEvery } from "redux-saga/effects";
 import { openModal, closeModal } from "./modal";
 import { AddFolder, handleDeleteFolder } from "../../api/folderApi";
 import { LikesFolderType } from "../../screens/Likes";
-import { handleDeleteProducts } from "../../api/folderApi";
+import {
+  handleDeleteProducts,
+  handleChangeProducts,
+} from "../../api/folderApi";
+import { FolderSagaAction } from "./folderTypes";
 import {
   pending,
   success_addFolder,
@@ -10,37 +14,14 @@ import {
   success_deleteFolder,
   success_deleteProducts,
   filterdLikes,
+  DELETE_PRODUCTS,
+  DELETE_FOLDER,
+  ADD_FOLDER,
+  CHANGE_FOLDER,
 } from "./folderActions";
-import { DeleteFolderSaga, FolderType } from "./folderTypes";
-import { DELETE_PRODUCTS, DELETE_FOLDER, ADD_FOLDER } from "./folderActions";
+import { DeleteFolderSaga, FolderType, ChangeFoldersSaga } from "./folderTypes";
 import { ProductItem } from "../../components/modal/LikeProductListModal";
-/* function* filterProductSaga(action: FolderSagaAction) {
-  try {
-    yield put(pending());
-    const { products } = yield select((state) => state.folder);
 
-    if (action.payload.changeFolder === originFolder) {
-      // alert 모달 추가 후 수정 예정
-      yield put(closeModal("likeDetail"));
-      yield put(
-        openModal("alert", {
-          message: "같은 폴더로는 상품을 옮길 수 없습니다.",
-        })
-      );
-      return;
-    }
-    // 서버 연동 할 함수 추가 예정
-    const { originFolder } = yield select((state) => state.folder);
-
-    const filterItems = products.filter(
-      (product: ProductItem) => !action.payload.productsId.includes(product.id)
-    );
-    yield put(success_deleteProducts(filterItems));
-  } catch (error: any) {
-    yield put(fail(error));
-  }
-}
- */
 function* addFolderSaga(action: FolderType) {
   try {
     yield put(pending());
@@ -73,25 +54,61 @@ function* deleteProductSaga(action: DeleteFolderSaga) {
   try {
     yield put(pending());
     const { products } = yield select((state) => state.folder);
-
-    const filteredProducts = products.filter((product: ProductItem) => {
-      return !action.payload.productId.includes(product.id); // 필터링된 상품들을 반환
-    });
-    yield call(
-      handleDeleteProducts,
-      action.payload.choiceName,
-      action.payload.productId
+    console.log(
+      products,
+      "기존상품들",
+      action.payload.productIdList,
+      "클릭된 상품리스트들"
     );
+    const filteredProducts = products.filter((product: ProductItem) => {
+      return !action.payload.productIdList.includes(product.id); // 필터링된 상품들을 반환
+    });
+
+    yield call(handleDeleteProducts, {
+      choiceName: action.payload.choiceName,
+      productIdList: action.payload.productIdList,
+    });
     yield put(success_deleteProducts(filteredProducts));
-    console.log("ㅇㅇㅇ", action.payload.productId);
-    yield put(filterdLikes(action.payload.productId));
+
+    yield put(filterdLikes(action.payload.productIdList));
   } catch (error: any) {
     yield put(fail(error));
   }
 }
+function* changeProductSaga(action: FolderSagaAction) {
+  try {
+    yield put(pending());
+    const { products } = yield select((state) => state.folder);
+
+    if (action.payload.changeName === action.payload.originName) {
+      // alert 모달 추가 후 수정 예정
+      yield put(closeModal("likeDetail"));
+      yield put(
+        openModal("alert", {
+          message: "같은 폴더로는 상품을 옮길 수 없습니다.",
+        })
+      );
+      return;
+    }
+
+    const filteredProducts = products.filter((product: ProductItem) => {
+      return !action.payload.productIdList.includes(product.id); // 필터링된 상품들을 반환
+    });
+
+    yield call(handleChangeProducts, {
+      originName: action.payload.originName,
+      changeName: action.payload.changeName,
+      productIdList: action.payload.productIdList,
+    });
+    yield put(success_deleteProducts(filteredProducts));
+  } catch (error: any) {
+    yield put(fail(error));
+  }
+}
+
 export function* watchFolderSaga() {
   yield takeEvery(DELETE_PRODUCTS, deleteProductSaga);
-  /*   yield takeEvery(CHANGE_FOLDER, filterProductSaga); */
+  yield takeEvery(CHANGE_FOLDER, changeProductSaga);
   yield takeEvery(DELETE_FOLDER, deleteFolderSaga);
   yield takeEvery(ADD_FOLDER, addFolderSaga);
 }
