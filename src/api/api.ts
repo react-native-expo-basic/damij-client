bf1585352c4cf2e8191a1eb19bbb08b24ea823e6
minseung-gang <strause1@nate.com>
2023-07-14 23:56:07 +0900

    chore: 필요없는 코드 삭제

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,18 +1,12 @@
 import axios from "axios";
 import TokenService from "../services/TokenSerivce";
 
-export const instance = axios.create({
-  baseURL: "http://13.124.181.25/",
-});
-
 export const authInstance = axios.create({
-  baseURL: "http://13.124.181.25/",
+  baseURL: "http://13.124.181.25",
 });
 authInstance.interceptors.request.use(async (config) => {
-  if (config.headers === undefined) console.log(config.headers);
   const token = await TokenService.get();
-
   config.headers.Authorization = token;
-  console.log(config.headers.Authorization);
+
   return config;
 });
