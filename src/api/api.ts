09715c09c8b57c5c8aedc8317c7a56910f54eb8c
minseung-gang <strause1@nate.com>
2023-07-15 01:22:27 +0900

    Fix: 게스트와 회원 구분하는 코드 추가

--- src/api/api.ts
+++ src/api/api.ts
@@ -5,7 +5,18 @@ export const authInstance = axios.create({
   baseURL: "http://13.124.181.25",
 });
 authInstance.interceptors.request.use(async (config) => {
-  const token = await TokenService.get();
+  let tokenType: "user" | "guest";
+
+  // 로직을 추가하여 토큰 타입을 결정
+  // 예: 비회원 토큰이 있는지 확인 후, 없으면 회원 토큰을 사용
+  const userToken = await TokenService.getToken("user");
+  if (userToken) {
+    tokenType = "user";
+  } else {
+    tokenType = "guest";
+  }
+
+  const token = await TokenService.getToken(tokenType);
   config.headers.Authorization = token;
 
   return config;
