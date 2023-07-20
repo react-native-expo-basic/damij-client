63e1f1b4c03bde462d23276f9c0266bb655348fb
minseung-gang <strause1@nate.com>
2023-07-20 23:07:06 +0900

    Fix: 토큰 스토리지에 저장하는 기능 추가

--- src/api/userApi.ts
+++ src/api/userApi.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
+import TokenService from "../services/TokenSerivce";
 
 export const fetchGuestToken = async () => {
   const response = await axios.post(
     `http://13.124.181.25/api/users/guestsignup`
   );
+  TokenService.setToken("guest", response.headers.guest);
   return response.headers.guest;
 };
