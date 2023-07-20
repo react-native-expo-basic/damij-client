b79d15b6e8a2f943ce0a5a9477e93ff5632b622d
minseung-gang <strause1@nate.com>
2023-07-20 23:06:14 +0900

    Fix: 게스트 토큰 발급받는 코드 추가

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,23 +1,27 @@
 import axios from "axios";
 import TokenService from "../services/TokenSerivce";
+import { useSelector } from "react-redux";
+import { fetchGuestToken } from "./userApi";
 
 export const authInstance = axios.create({
   baseURL: "http://13.124.181.25",
 });
 authInstance.interceptors.request.use(async (config) => {
-  let tokenType: "user" | "guest";
-
-  // 회원의 토큰이 있는지 확인하고 없으면 게스트 토큰을 사용
   const userToken = await TokenService.getToken("user");
+  const guestToken = await TokenService.getToken("guest");
+
   if (userToken) {
-    tokenType = "user";
-    await TokenService.checkTokenExpiration(userToken);
-  } else {
-    tokenType = "guest";
+    config.headers.Authorization = userToken;
+    return config;
   }
 
-  const token = await TokenService.getToken(tokenType);
-  config.headers.Authorization = token;
+  if (userToken == null && guestToken == null) {
+    const guestToken = await fetchGuestToken();
+    console.log(guestToken);
+    config.headers.Authorization = guestToken;
+    return config;
+  }
 
+  config.headers.Authorization = guestToken;
   return config;
 });
