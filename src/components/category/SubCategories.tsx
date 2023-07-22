5d46dddc884d474f679113b7b0a43caaf003d88c
minseung-gang <strause1@nate.com>
2023-07-22 23:24:49 +0900

    Update: 카테고리 리스트 추가 및 적용

--- src/components/category/SubCategories.tsx
+++ src/components/category/SubCategories.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { View, Text } from "react-native";
 import styled from "styled-components/native";
-import { fetchCategoryType } from "../../screens/Category";
 
 export interface SubCategoryType {
   category: {
@@ -10,39 +9,59 @@ export interface SubCategoryType {
 }
 
 export default function SubCategories({ category }: SubCategoryType) {
-  const subCategories = {
+  // 영어로 되어있는 키의 값을 한글로 변환
+  const subCategories: { [key: string]: string } = {
+    manToMan: "맨투맨",
+    blouse: "블라우스",
+    shirt: "셔츠",
+    sleeveless: "민소매",
+    neat: "니트",
+    "t-shirt": "티셔츠",
+    pants: "팬츠",
+    skirt: "스커트",
+    onePiece: "원피스",
+    padding: "패딩",
+    mustang: "무스탕",
+    set: "세트",
+    pajamas: "파잠",
+    gown: "가운",
     best: "조끼",
     cardigan: "가디건",
     coat: "코트",
-    jacekt: "자켓",
+    jacket: "자켓",
   };
   return (
-    <>
+    <Wrapper>
       {Object.entries(category).map(([category, image], idx) => {
+        const categoryName = subCategories[category];
         return (
           <Container key={idx}>
             <CategoryImage source={{ uri: image.trim() }} />
-            <CategoryText>{category}</CategoryText>
+            <CategoryText>{categoryName}</CategoryText>
           </Container>
         );
       })}
-    </>
+    </Wrapper>
   );
 }
-
+const Wrapper = styled.View`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  width: 30%;
+  margin: 3% 1.5%;
+`;
 const Container = styled.View`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  padding: 5px;
-  width: 31%;
-  margin: 3% 1.5%;
+  width: 100%;
   border-radius: 50px;
   aspect-ratio: 1/1;
   background: white;
   border-width: 1px;
-  padding: 3px;
+  padding: 5px;
   border-color: #d9d9d92f;
 `;
 const CategoryImage = styled.Image`
