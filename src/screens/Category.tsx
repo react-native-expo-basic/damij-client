c0cbad3f94a673837188196c7e9b1462636e9c0b
minseung-gang <strause1@nate.com>
2023-07-22 23:27:48 +0900

    Update:  FlatList 감지할 ref 추가 및 해당 offset 이동 기능 추가

--- src/screens/Category.tsx
+++ src/screens/Category.tsx
@@ -1,21 +1,25 @@
-import React, { useState, useCallback, useEffect } from "react";
-import { FlatList } from "react-native";
+import React, { useState, useEffect, useRef } from "react";
+import { FlatList, View } from "react-native";
 import styled from "styled-components/native";
 import CategoryMenu from "../components/category/CategoryMenu";
 import { authInstance } from "../api/api";
 import CategoryList from "../components/category/CategoryList";
 
 export interface fetchCategoryType {
-  [key: string]: {
-    [key: string]: string;
-  };
+  [key: string]: { [key: string]: string };
 }
 
 export default function Category() {
   const [categoryList, setCateogryList] = useState<fetchCategoryType[]>([]);
-  const [offset, setOffset] = useState(0);
-
-  const filteredItems = useCallback(async () => {
+  const [selectedButton, setSelectedButton] = useState("상의");
+  const flatListRef = useRef<FlatList>(null);
+  const superCategories: { [key: string]: string } = {
+    TOP: "상의",
+    BOTTOM: "하의",
+    OUTER: "아우터",
+    HOME_WEAR: "홈웨어",
+  };
+  const filteredItems = async () => {
     try {
       const response = await authInstance.get("/api/product/categorylist");
       const data: fetchCategoryType = response.data;
@@ -32,23 +36,53 @@ export default function Category() {
     } catch (error) {
       console.log(error);
     }
-  }, []);
+  };
 
   useEffect(() => {
     filteredItems();
   }, []);
 
+  useEffect(() => {
+    scrollToCategory(selectedButton);
+  }, [selectedButton]);
+
+  // 선택한 카테고리로 스크롤하는 함수
+  const scrollToCategory = (category: string) => {
+    const superCategoryKey = Object.keys(superCategories).find(
+      (key) => superCategories[key] === category
+    );
+
+    if (superCategoryKey) {
+      // 스크롤할 인덱스 찾기
+      const index = categoryList.findIndex(
+        (item) => Object.keys(item)[0] === superCategoryKey
+      );
+
+      // 스크롤 실행
+      if (flatListRef.current && index !== -1) {
+        flatListRef.current.scrollToIndex({ animated: true, index });
+      }
+    }
+  };
   return (
     <Wrapper>
-      <CategoryMenu setOffset={setOffset} />
+      <CategoryMenu
+        setSelectedButton={setSelectedButton}
+        selectedButton={selectedButton}
+      />
       <FlatList
         data={categoryList}
+        ref={flatListRef}
         keyExtractor={(item, index) => index.toString()}
-        renderItem={({ item }) => <CategoryList item={item} />}
+        initialScrollIndex={0}
+        renderItem={({ item }) => (
+          <CategoryList item={item} superCategories={superCategories} />
+        )}
       />
     </Wrapper>
   );
 }
+
 const Wrapper = styled.View`
   display: flex;
   flex-direction: row;
